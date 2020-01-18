b24564221b5d3494a37a1791a5e01f8d9d0b5b1e
zamhadi21 <zamhadi21@gmail.com>
2020-01-18 17:51:40 +0700

    first commit

---
+++ nomor2.php
@@ -0,0 +1,14 @@
+<?php
+
+$quick = 
+
+["Keren"=>14,
+" D"=>1,"W"=>5,"B"=>4,"T"=>11,"A"=>6,"S"=>8,"U"=>2,"D"=>9,"M"=>3,"O"=>10,"Y"=>7,"T"=>11,"I"=>12,"D "=>13
+, " Sekali"=>15];
+
+asort($quick);
+foreach($quick as $kata=>$angka) {
+    echo $kata;
+}
+ 
+?>
